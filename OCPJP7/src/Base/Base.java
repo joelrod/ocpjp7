90933cd0cb7c9f0e72f3c52cc89ed56b4a92505d
jorodriguez <jorodriguez@ihsa.mx>
2016-05-10 00:16:43 -0500

    e agrego la clase BASE

--- OCPJP7/src/Base/Base.java
+++ OCPJP7/src/Base/Base.java
@@ -10,5 +10,57 @@ package Base;
  * @author jorodriguez
  */
 public class Base {
-    
+
+    public static void foo(Base bObj) {
+        System.out.println("In Base.foo()");
+        bObj.bar();
+    }
+
+    public void bar() {
+        System.out.println("In Base.bar()");
+    }
 }
+
+class Derived extends Base {
+
+    public static void foo(Base bObj) {
+        System.out.println("In Derived.foo()");
+        bObj.bar();
+    }
+
+    public void bar() {
+        System.out.println("In Derived.bar()");
+    }
+}
+
+class OverrideTest {
+
+    public static void main(String[] args) {
+        Base bObj = new Derived();
+        bObj.foo(bObj);
+    }
+}
+
+/*
+ a)
+ In Base.foo()
+ In Base.bar()
+ b)
+ In Base.foo()
+ In Derived.bar()
+ c)
+ In Derived.foo()
+ In Base.bar()
+ d)
+ In Derived.foo()
+ In Derived.bar()
+
+ my answer :
+
+ B)
+
+
+result
+
+
+ */
